0e0f24ecf0df195e2610ed35bc83ed3bfc56385e
baujuncos <baujuncos@gmail.com>
2025-11-12 17:41:12 -0300

    Corregimos error de proxy

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,11 +3,8 @@
 # Default to QA if BACKEND_URL is not set
 BACKEND_URL=${BACKEND_URL:-https://tiktask-backend-qa.onrender.com}
 
-# Extract host from URL
-BACKEND_HOST=$(echo $BACKEND_URL | sed -e 's|https\?://||' -e 's|/.*||')
-
 # Replace variables in nginx config
-envsubst '${BACKEND_URL} ${BACKEND_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g 'daemon off;'
